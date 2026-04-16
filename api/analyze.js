467ae69c00d76619dcbf91eb582719e82480226b
hummingway1 <taekwangs@gmail.com>
2026-04-16 21:42:56 +0800

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,4 +1,5 @@
-// v2
+// debug
+export const config = { runtime: 'nodejs' };
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
